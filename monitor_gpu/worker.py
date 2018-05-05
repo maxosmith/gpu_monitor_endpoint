86e59b7138e33c8036173f2ce469c6ce675153f3
MaxOSmith <Max.Olan.Smith@gmail.com>
2018-05-05 16:20:37 -0400

    Update worker.py

--- monitor_gpu/worker.py
+++ monitor_gpu/worker.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def main():
     stats = gpustat.new_query()
     cpu_usage = commands.getstatusoutput(
-        "grep 'cpu ' /proc/stat | awk '{{usage=($2+$4)*100/($2+$4+$5)}} END {{print usage \"%\"}}'")[1]
+        "op -b -n2 -p 1 | fgrep \"Cpu(s)\" | tail -1 | awk -F'id,' -v prefix=\"$prefix\" '{{ split($1, vs, \",\"); v=vs[length(vs)]; sub(\"%\", \"\", v); printf \"%s%.1f%%\n\", prefix, 100 - v }}'")[1]
 
     data_json = stats.jsonify()
     data_json["cpu.usage"] = cpu_usage
